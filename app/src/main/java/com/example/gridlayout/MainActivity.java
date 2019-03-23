9428d29ff8bda578e181baf9690dd752c8d0369d
jawadahmed <jawadahmedtaj@gmail.com>
2019-03-23 22:38:37 +0100

    Test

--- app/src/main/java/com/example/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -1,9 +1,26 @@
 package com.example.gridlayout;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonTapped(View view){
+
+        int id = view.getId();
+        String ourID = "";
+
+        ourID = view.getResources().getResourceEntryName(id);
+
+        int resourceID = getResources().getIdentifier(ourID, "raw", "com.example.gridLayout");
+
+        MediaPlayer mPlayer = MediaPlayer.create(this, resourceID);
+        mPlayer.start();
+
+        System.out.println("button tapped" + ourID);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
